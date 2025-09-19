a8385c99d1693687fdbdc1be58869db509161b8a
ZhenyaTumarkin <zh.tumarkin@gmail.com>
2025-09-19 13:24:00 +0200

    slurm single and batch script

--- code/preprocessing/bash_scripts/launch_array.sh
+++ code/preprocessing/bash_scripts/launch_array.sh
@@ -5,19 +5,20 @@
 
 ##  Defining options for slurm how to run
 #----------------------------------------------------------------
-#SBATCH --job-name=arraySlurmExpl
-#SBATCH --output=array_%A-%a.log # %A and %a are placeholders for the jobid and taskid, resp.
-
+#SBATCH --job-name=Glacier_arr_preprocess_test
+#SBATCH --output=/nfs/scistore18/pelligrp/etumarki/HMA_sensitivity/outlog/Glaconetes_%A-%a.log 
+# %A and %a are placeholders for the jobid and taskid, resp.
 # Number of CPU cores to use within one node
 #SBATCH -c 1
 
 # Define the number of hours the job should run. 
 # Maximum runtime is limited to 10 days, ie. 240 hours
-#SBATCH --time=0:05:00
+#SBATCH --time=00-00:10
+#SBATCH --constraint=matlab
 
 # Define the amount of RAM used by your job in GigaBytes
 # In shared memory applications this is shared among multiple CPUs
-#SBATCH --mem=1G
+#SBATCH --mem=4G
 
 # Do not requeue the job in the case it fails.
 #SBATCH --no-requeue
@@ -35,4 +36,14 @@ unset SLURM_EXPORT_ENV
 # in different iteration of the script execution
 #----------------------------------------------------------------
 
-srun 
+csv_file="/nfs/scistore18/pelligrp/etumarki/HMA_sensitivity/data/preprocessing/Glacier_list.csv"
+
+# Skip header if needed: tail -n +2
+first_col=()
+while IFS= read -r value; do
+    first_col+=("$value")
+done < <(tail -n +4 "$csv_file" | cut -d',' -f2)
+
+rgiid=${first_col[${SLURM_ARRAY_TASK_ID}]}
+
+srun --cpu_bind=verbose  bash /nfs/scistore18/pelligrp/etumarki/HMA_sensitivity/code/preprocessing/bash_scripts/preprocessing.sh $rgiid
